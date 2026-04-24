bf9650aff33944907b3ba5a5f1752c090c4ab21a
Hridoy Hasan Babu <hridoyakanda100@gmail.com>
2026-04-24 18:13:08 +0600

    again updated

---
+++ src/app/singup/page.jsx
@@ -0,0 +1,12 @@
+import {Check} from "@gravity-ui/icons";
+import {Button, Description, FieldError, Form, Input, Label, TextField} from "@heroui/react";
+
+const SingUppage = () => {
+    return (
+        <div>
+            <h1 className='text-4xl text-white font-bold'>Sing UP form hare</h1>
+        </div>
+    );
+};
+
+export default SingUppage;
